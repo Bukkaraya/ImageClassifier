6c22492d4506be98078d12bc0f95036e6f5c384f
Abinav <Abinavbukkaraya@gmail.com>
2018-01-14 17:31:54 -0500

    Added softmax activation and changed cost function

--- neural.py
+++ neural.py
@@ -2,6 +2,28 @@ import numpy as np
 
 np.seterr(all='log')
 
+
+def softmax_activation(Z):
+    cache = Z
+
+    shiftz = Z - np.max(Z, axis=0)
+    A = np.exp(shiftz) / np.sum(np.exp(shiftz), axis=0)
+
+    assert A.shape == Z.shape
+
+    return A, cache
+
+def softmax_backward(dA, cache):
+    Z = cache
+
+    shiftz = Z - np.max(Z, axis=0)
+    softmax = np.exp(shiftz) / np.sum(np.exp(shiftz), axis=0)
+    dZ = dA * softmax * (1 - softmax)
+
+    assert dZ.shape == Z.shape
+
+    return dZ
+
 def relu_activation(Z):
     cache = Z
     A = np.maximum(0, Z)
@@ -33,6 +55,10 @@ def sigmoid_backward(dA, cache):
     assert dZ.shape == Z.shape
 
     return dZ
+
+
+def softmax(y):
+    return np.exp(y) / np.sum(np.exp(y), axis=0)
 
 def encode_one_hot(targets, num_classes):
     """
@@ -104,6 +130,7 @@ def initialize_parameters(layer_dims):
 
 def one_layer_forward(W, b, act_prev):
     Z = np.dot(W, act_prev) + b
+
     assert Z.shape == (W.shape[0], act_prev.shape[1])
 
     return Z, (act_prev, W, b)
@@ -114,6 +141,11 @@ def one_layer_activation(W, b, act_prev, activation_type):
         activation, act_cache = relu_activation(Z)
     if activation_type == 'sigmoid':
         activation, act_cache = sigmoid_activation(Z)
+    if activation_type == 'softmax':
+        activation, act_cache = softmax_activation(Z)
+        print('W' + str(W[0:4]))
+        print('b' + str(b[0:4]))
+        print('act_prev' + str(act_prev[0:4]))
 
     return activation, (lin_cache, act_cache)
 
@@ -127,7 +159,7 @@ def forward_propagation(X_train, parameters):
         caches.append(cache)
         act_prev = A
     
-    A_final, cache = one_layer_activation(parameters["W"+str(num_layers)], parameters["b"+str(num_layers)], act_prev, "sigmoid")
+    A_final, cache = one_layer_activation(parameters["W"+str(num_layers)], parameters["b"+str(num_layers)], act_prev, "softmax")
     caches.append(cache)
     assert A_final.shape == (10, X_train.shape[1])
 
@@ -136,9 +168,9 @@ def forward_propagation(X_train, parameters):
 def get_cost(A_final, Y_train):
     m = Y_train.shape[1]
 
-    cost = (-1/m) * np.sum(np.multiply(Y_train, np.log(A_final)) + np.multiply((1 - Y_train), np.log(1 - A_final)))
+    cost = (-1/m) * np.sum(np.sum(np.multiply(Y_train, np.log(A_final))))
     cost = np.squeeze(cost)
-    
+
     assert cost.shape == ()
 
     return cost
@@ -165,6 +197,8 @@ def activation_backward(dA, cache, activation_type):
         dZ = relu_backward(dA, act_cache)
     elif activation_type == 'sigmoid':
         dZ = sigmoid_backward(dA, act_cache)
+    elif activation_type == 'softmax':
+        dZ = softmax_backward(dA, act_cache)
 
     dW, db, dA_prev = one_layer_backward(dA, lin_cache)
 
@@ -177,10 +211,10 @@ def backward_propagation(A_final, Y_train, caches):
     m = A_final.shape[1]
     Y_train = Y_train.reshape(A_final.shape)
 
-    dA_final = - (np.divide(Y_train, A_final) - np.divide((1 - Y_train), (1 - A_final)))
+    dA_final = - np.divide(Y_train, A_final)
 
     current_cache = caches[num_layers - 1]
-    a, b, c = activation_backward(dA_final, current_cache, 'sigmoid')
+    a, b, c = activation_backward(dA_final, current_cache, 'softmax')
     gradients["dW"+str(num_layers)] = a
     gradients["db"+str(num_layers)] = b
     gradients["dA"+str(num_layers)] = c
@@ -205,15 +239,60 @@ def update_parameters(parameters, gradients, learning_rate=0.05):
 
     return parameters
 
-
-if __name__ == '__main__':
+def train_model(num_iterations):
     X_train, Y_train, X_test, Y_test = get_data()
-    paramters = initialize_parameters([X_train.shape[0], 128, 128, 128, 128, 10])
-    num_iterations = 101
+    parameters = initialize_parameters([X_train.shape[0], 256, 256, 256, 256, 10])
+
 
     for i in range(num_iterations):
         print('Iteration ' + str(i))
-        A_final, caches = forward_propagation(X_train, paramters)
-        print(get_cost(A_final, Y_train))
+        A_final, caches = forward_propagation(X_train, parameters)
+        if i % 20 == 0:
+            print(get_cost(A_final, Y_train))
+            train_result = test_model(X_train, Y_train, parameters)
+            test_result = test_model(X_test, Y_test, parameters)
+
+            print('Training set accuracy: ' + str(train_result))
+            print('Test set accuracy:' + str(test_result))
         grads = backward_propagation(A_final, Y_train, caches)
-        parameters = update_parameters(paramters, grads)
+        parameters = update_parameters(parameters, grads)
+
+    return parameters
+
+
+def test_model(X, Y, parameters):
+    num_layers = len(parameters) // 2
+    correct = 0
+    p, _ = forward_propagation(X, parameters)
+
+    for i in range(Y.shape[1]):
+        p[:,i] = p[:,i] >= np.max(p[:,i])
+        if np.array_equal(p[:,i], Y[:,i]):
+            correct += 1
+
+    return correct / Y.shape[1]
+
+
+def dump_parameters(params):
+    import pickle
+    with open('params.pickle', 'wb') as fs:
+        pickle.dump(params, fs)
+
+
+def get_params():
+    import pickle
+    d = None
+    with open('params.pickle', 'rb') as fo:
+        d = pickle.load(fo)
+
+    return d
+
+if __name__ == '__main__':
+    X_train, Y_train, X_test, Y_test = get_data()
+    trained_parameters = train_model(1001)
+    dump_parameters(trained_parameters)
+    train_result = test_model(X_train, Y_train, trained_parameters)
+    test_result = test_model(X_test, Y_test, trained_parameters)
+
+    print('Training set accuracy: ' + str(train_result))
+    print('Test set accuracy:' + str(test_result))
