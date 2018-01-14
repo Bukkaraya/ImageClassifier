a0cad93cc9550533fc1b6f9ac5d8160707324980
Abinav <Abinavbukkaraya@gmail.com>
2018-01-13 19:24:11 -0500

    Implemented function to initialize parameters for the model and added some docstrings

--- neural.py
+++ neural.py
@@ -2,6 +2,12 @@ import numpy as np
 
 
 def encode_one_hot(targets, num_classes):
+    """
+        Function used to convert a numpy array with labels to a feature/class vector
+        Arguments:
+        targets - the array that needs to be encoded
+        num_classes - number for rows/classes in the resulting vector
+    """
     return np.eye(num_classes)[np.array(targets)]
 
 def get_data():
@@ -37,15 +43,34 @@ def get_data():
 
 
     #Make sure the shapes we have of the train and test sets are correct
-    assert(X_train.shape == (50000, 3072))
-    assert(Y_train.shape == (50000, 10))
-    assert(X_test.shape == (10000, 3072))
-    assert(Y_test.shape == (10000, 10))
+    assert X_train.shape == (50000, 3072)
+    assert Y_train.shape == (50000, 10)
+    assert X_test.shape == (10000, 3072)
+    assert Y_test.shape == (10000, 10)
 
     return X_train, Y_train, X_test, Y_test
 
+def initialize_parameters(layer_dims):
+    """
+        Initialized the parameters for the neural network
+    """
+    parameters = {}
+    num_layers = len(layer_dims)
+    np.random.seed(1)
 
+    for l in range(1, num_layers):
+        parameters["W" + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * 0.01
+        parameters["b" + str(l)] = np.zeros((layer_dims[l], 1))
+
+        print(parameters["W"+str(l)].shape)
+        print(parameters["b"+str(l)].shape)
+        assert parameters["W"+str(l)].shape == (layer_dims[l], layer_dims[l - 1])
+        assert parameters["b"+str(l)].shape == (layer_dims[l], 1)
+
+    return parameters
 
 
 if __name__ == '__main__':
-    get_data()
+    X_train, Y_train, X_test, Y_test = get_data()
+    initialize_parameters([X_train.shape[1], 128, 128, 128, 128, 10])
+
